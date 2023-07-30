838e51f0465c22cb3ba906cfb45498f50aa240c4
OmarTarek15 <81865160+OmarTarek15@users.noreply.github.com>
2023-07-30 19:31:02 +0300

    Add styling and separate normal and work lists

--- index.js
+++ index.js
@@ -3,19 +3,46 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-var taskList = [];
+
+const taskList = ["Do the dishes", "Buy food", "Learn to code", "Play games"];
+const workList = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.post("/submit", (req, res) => {
-  var newTask = req.body["newtask"];
-  taskList.push(newTask);
-  res.redirect("/");
-});
+function getDate() {
+  const date = new Date();
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return date.toLocaleDateString("en-US", options);
+}
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { tasks: taskList });
+  const day = getDate();
+  res.render("index.ejs", { title: day, tasks: taskList });
+});
+
+app.get("/work", (req, res) => {
+  res.render("index.ejs", { title: "Work List", tasks: workList });
+});
+
+app.post("/", (req, res) => {
+  const newTask = req.body.newtask;
+  console.log(req.body);
+
+  if (req.body.list === "Work") {
+    workList.push(newTask);
+    res.redirect("/work");
+  } else {
+    taskList.push(newTask);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
